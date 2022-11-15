7a80be898e20f455463d21ac68d637d3b2a1551c
Mahendra Dwi Purwanto <mahendradwipurwanto@gmail.com>
2022-11-15 11:15:54 +0700

    login

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,6 +8,17 @@ class Admin extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        if ($this->session->userdata('logged_in') == false || !$this->session->userdata('logged_in')) {
+            if (!empty($_SERVER['QUERY_STRING'])) {
+                $uri = uri_string() . '?' . $_SERVER['QUERY_STRING'];
+            } else {
+                $uri = uri_string();
+            }
+            $this->session->set_userdata('redirect', $uri);
+            $this->session->set_flashdata('notif_warning', "Harap login, untuk melanjutkan");
+            redirect('login');
+        }
+
         $this->load->model(['M_admin', 'M_kategori', 'M_kriteria']);
     }
 
